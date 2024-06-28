44a6df4202ec8d590b5ff05f41dfb0dfd181602b
Jack Gregory <jackgregory@gmail.com>
2024-06-28 15:41:24 +0200

    Move Stata regression functions to <src>

--- build/03_analysis/3_regs.R
+++ build/03_analysis/3_regs.R
@@ -11,6 +11,10 @@
 
 # PREAMBLE ----------------------------------------------------------------------------------------
 
+## Initiate regression functions
+source(here("src/stata_regs.R"))
+
+
 ## Create output folder
 date <- format(Sys.Date(), "%Y%m%d")
 # date <- "20221117"
@@ -19,114 +23,6 @@ dir_create(path(l.path$out, date))
 
 # MAIN REGRESSIONS --------------------------------------------------------------------------------
 
-## stata_reg ------------------------------------------------------------------
-
-## fml = formula as a list of stata variables concatenated by spaces
-## df = dataframe containing regression data
-## type = regression type in set {reg, iv}
-
-## Method set & use function
-stata_reg <- function(fml, df, type, bootstrap=FALSE) {
-  
-  ## Assertions
-  stopifnot(
-    is.character(fml),
-    is.data.frame(df),
-    is.character(type),
-    type %in% c("reg","iv"),
-    is.logical(bootstrap)
-  )
-  if (type=="reg" && bootstrap==TRUE) {
-    stop('The bootstrap option can only be applied with the "iv" type.')
-  }
-  
-  ## Method
-  if (type=="iv") {
-    fml <- structure(fml, class=c(class(fml), "iv"))
-  } else {
-    fml <- structure(fml, class=c(class(fml), "reg"))
-  }
-  
-  ## Use method
-  UseMethod("stata_reg", fml)
-}
-
-## Method: reg
-stata_reg.reg <- function(fml, df, ...) {
-  
-  cat("\nstata::reg  ", fml, "\n", sep="")
-  
-  ## Create Stata call
-  stata_do <- 
-    glue('eststo: reg {fml}, vce(robust)
-          regsave, tstat pval ci detail(scalars)
-         ')
-  
-  ## Create filter based on LHS var
-  if (str_detect(fml, "^survive")) {
-    filter_survive <- expr(year<=2017)
-  } else {
-    filter_survive <- expr(TRUE)
-  }
-  
-  ## Perform regression
-  stata(stata_do,
-        data.in = filter(df, !!filter_survive),
-        data.out = TRUE)
-}
-
-## Method: iv
-stata_reg.iv <- function(fml, df, ..., bootstrap) {
-  
-  cat("\nstata::iv  ", fml, "\n", sep="")
-  
-  ## Create Stata call
-  fml <- str_split(fml, " ", n=2, simplify=TRUE) %>%
-    as.list() %>%
-    set_names(nm=c("lhs","rhs"))
-  if (bootstrap) {
-    stata_do <-
-      glue("cap program drop gf_bs
-            program define gf_bs, rclass
-            
-              cap drop sulf_cont_iv ARP_iv_sulf_cont
-          
-            	reg sulfur_content_tot sulfur_net_iv {fml$rhs}, vce(cluster ID)
-            	predict sulf_cont_iv
-            	gen ARP_iv_sulf_cont = sulf_cont_iv * ARPprice
-          	
-            	reg {fml$lhs} sulf_cont_iv ARP_iv_sulf_cont {fml$rhs}, vce(cluster ID)
-            
-            end
-            
-            bootstrap _b, reps(100) seed(100): gf_bs
-            regsave, tstat pval ci detail(scalars)
-           ")
-  } else {
-    stata_do <- 
-      glue("eststo: reg sulfur_content_tot sulfur_net_iv {fml$rhs}, vce(cluster ID)
-            predict sulf_cont_iv
-            gen ARP_iv_sulf_cont = sulf_cont_iv * ARPprice
-  
-            eststo: reg {fml$lhs} sulf_cont_iv ARP_iv_sulf_cont {fml$rhs}, vce(cluster ID)
-            regsave, tstat pval ci detail(scalars)
-           ")
-  }
-  
-  ## Create filter based on LHS var
-  if (fml$lhs=="survive") {
-    filter_survive <- expr(year<=2017)
-  } else {
-    filter_survive <- expr(TRUE)
-  }
-  
-  ## Perform regression
-  stata(stata_do,
-        data.in = filter(df, !!filter_survive),
-        data.out = TRUE)
-}
-
-  
 ## Build specifications -------------------------------------------------------
 
 ## Left-hand side vars
